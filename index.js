b982afb4afd1414cb1ce081954ab05d2553750bf
Krystian Bieniek <krystekk2013@gmail.com>
2019-02-22 12:12:18 +0100

    Serwer HTTML

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 var http = require('http');
-// var server = http.createServer(function (request, response) {
-
-// });
+var fs = require('fs');
 
 var server = http.createServer();
 
 server.on('request', function (request, response) {
-  response.setHeader("Content-Type", "text/html; charset=utf-8");
   if (request.method === 'GET' && request.url === '/hello') {
-    response.write('<h1>Hello World!</h1>');
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    fs.readFile('./index.html', 'utf-8', function(err, data) {
+      response.write(data);
       response.end();
+    });
   } else {
-      response.statusCode = 404;
-      response.write('<h1>404: Zła ścieżka!</h1>');
-      response.end();
+      fs.readFile('./404.jpg', function(err, data) {
+        response.statusCode = 404;
+        response.write(data);
+        response.end();
+      });
     }
 });
 
